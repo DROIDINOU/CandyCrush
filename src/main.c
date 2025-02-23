5fb96d5739d5029f9b511e194470cfc076504f37
DROIDINOU <76789720+DROIDINOU@users.noreply.github.com>
2025-02-23 23:55:07 +0100

    update

--- src/main.c
+++ src/main.c
@@ -12,21 +12,24 @@
 // $ gcc main.c matrice.c constante.c queue.c affichage.c -o mon_programme -lraylib
 // Ajoute RayGUI plus tard
 
-int main() {
-    srand(time(NULL)); // Initialisation du générateur de nombres aléatoires
+int main()
+{
+    srand(time(NULL));                         // Initialisation du générateur de nombres aléatoires
     InitWindow(800, 800, "Candy Crush Clone"); // 🖥️ Création de la fenêtre Raylib
     SetTargetFPS(60);
 
     // Chargement des textures
     Texture2D textures[5];
-    textures[0] = LoadTexture("candyimages/candy-sprite.png");  // Violet (M)
+    textures[0] = LoadTexture("candyimages/candy-sprite.png");       // Violet (M)
     textures[1] = LoadTexture("candyimages/poignee_de_bonbons.png"); // Rouge (R)
-    textures[2] = LoadTexture("candyimages/CANDYCANEPOLE.png"); // Bleu (B)
+    textures[2] = LoadTexture("candyimages/CANDYCANEPOLE.png");      // Bleu (B)
     textures[3] = LoadTexture("candyimages/poignee_de_bonbons.png"); // Vert (V)
-    textures[4] = LoadTexture("candyimages/candy-sprite.png"); // Jaune (J)
+    textures[4] = LoadTexture("candyimages/candy-sprite.png");       // Jaune (J)
 
-    for (int i = 0; i < 5; i++) {
-        if (textures[i].id == 0) {
+    for (int i = 0; i < 5; i++)
+    {
+        if (textures[i].id == 0)
+        {
             printf("Erreur de chargement de la texture %d\n", i);
         }
     }
@@ -43,7 +46,8 @@ int main() {
     // Initialiser la grille avant la boucle
     initialiser_grille(&maGrille);
 
-    while (!WindowShouldClose() && niveau < 1) { // Boucle principale Raylib
+    while (!WindowShouldClose() && niveau < 1)
+    { // Boucle principale Raylib
         BeginDrawing();
         ClearBackground(RAYWHITE);
 
@@ -55,35 +59,50 @@ int main() {
         bool isVerificationInit = false;
 
         Actions action;
-        while (q.taille > 0 && strcmp((action = defiler(&q)).actionName, "FINNIVEAU") != 0) {
-            if (strcmp(action.actionName, "LECTURE") == 0) {
+        while (q.taille > 0 && strcmp((action = defiler(&q)).actionName, "FINNIVEAU") != 0)
+        {
+            if (strcmp(action.actionName, "LECTURE") == 0)
+            {
                 int ligne = ObtenirReponseAuMessage(MESSAGEETREPONSESATTENDUES, 3);
                 int colonne = ObtenirReponseAuMessage(MESSAGEETREPONSESATTENDUES, 3);
                 int ligne1 = ObtenirReponseAuMessage(MESSAGEETREPONSESATTENDUES, 3);
                 int colonne1 = ObtenirReponseAuMessage(MESSAGEETREPONSESATTENDUES, 3);
                 LirePionsAChanger(&maGrille, ligne, colonne, ligne1, colonne1, &q);
-            } else if (strcmp(action.actionName, "CALCUL") == 0) {
+            }
+            else if (strcmp(action.actionName, "CALCUL") == 0)
+            {
                 Calcul(&q, &maGrille, action.pion1.x, action.pion1.y, action.pion2.x, action.pion2.y, &isVerificationInit);
-            } else if (strcmp(action.actionName, "SUPPRESSIONV") == 0) {
+            }
+            else if (strcmp(action.actionName, "SUPPRESSIONV") == 0)
+            {
                 SuppressionV(&maGrille, action.pion1.x, action.pion1.y, action.pion2.x, action.pion2.y, &q);
-            } else if (strcmp(action.actionName, "SUPPRESSIONH") == 0) {
+            }
+            else if (strcmp(action.actionName, "SUPPRESSIONH") == 0)
+            {
                 SuppressionH(&maGrille, action.pion1.x, action.pion1.y, action.pion2.x, action.pion2.y, &q);
-            } else if (strcmp(action.actionName, "VERIFICATION") == 0) {
+            }
+            else if (strcmp(action.actionName, "VERIFICATION") == 0)
+            {
                 Verification(&maGrille, &q);
-            } else if (strcmp(action.actionName, "DEPLACEMENT") == 0) {
+            }
+            else if (strcmp(action.actionName, "DEPLACEMENT") == 0)
+            {
                 Deplacement(&q, &maGrille, action.pion1.x, action.pion1.y, action.pion2.x, action.pion2.y);
-            } else if (strcmp(action.actionName, "INITIALISATION") == 0) {
+            }
+            else if (strcmp(action.actionName, "INITIALISATION") == 0)
+            {
                 initialiser_grille(&maGrille);
                 VerificationInitit(&q, &maGrille, &isVerificationInit);
             }
         }
 
         EndDrawing(); // Fin du rendu graphique
-        niveau++; // Passer au niveau suivant
+        niveau++;     // Passer au niveau suivant
     }
 
     // Décharger les textures après usage
-    for (int i = 0; i < 5; i++) {
+    for (int i = 0; i < 5; i++)
+    {
         UnloadTexture(textures[i]);
     }
 
