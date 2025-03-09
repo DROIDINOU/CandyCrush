1f915dbc2c5819f6f681c1717102bc7048ce6a82
DROIDINOU <76789720+DROIDINOU@users.noreply.github.com>
2025-03-09 20:18:59 +0100

    added const messageetatjeu

--- src/constante.c
+++ src/constante.c
@@ -2,13 +2,18 @@
 
 /*________________________________________________________________________________________________________________
                                  **** CONSTANTES
-     -> Tableau de messages et de réponses attendues
-     -> Parametres : NOMBREMESSAGES - MAXLONGUEUR
+     -> MESSAGEETREPONSESATTENDUES : Tableau de messages et de réponses attendues
+        * utilite : stocker les messages et les reponses attendues
+
+     -> MESSAGEETATJEU : Tableau des messages de l'etat du jeu
+        * utilite : stocker les messages de l'etat du jeu
+
      -> Structure des Niveaux
-     -> Elements : coupsNiveau - obstacleNiveau - compteurNiveau
+        * Elements : coupsNiveau - obstacleNiveau - compteurNiveau
                -> coupsNiveau : nombre de coups à jouer (int) - coups joués (int)
                -> obstacleNiveau : type d'obstacle (char) - nombre de gelatines (int)
                -> compteurNiveau : niveau actuel (int)
+        * utilite : stocker les informations sur les niveaux
 _____________________________________________________________________________________________
  */
 
@@ -17,6 +22,12 @@ const char MESSAGEETREPONSESATTENDUES[NOMBREMESSAGES][3][MAXLONGUEUR] = {
     {"\nMode affichage simple ou complet?", "S", "C"},
     {"Veuillez entrer la colonne (a, b, c, d, e, f, g, h) : ", "abcdefgh"},
     {"Veuillez entrer la ligne (1,2,3,4,5,6,7,8) : ", "12345678"}};
+
+const char MESSAGEETATJEU[NOMBREMESSAGESJEU][MAXLONGUEUR] = {
+    "Vous avez épuisé tous vos coups. Fin de la partie.",
+    "Vous avez gagné. Félicitations!",
+    " Votre score est de :",
+    "Vous avez perdu. Essayez encore."};
 
 Niveaux niveaux[FINALNIVEAU] = {
     {{3, 0}, {"Gelatine", 2}, 0},
