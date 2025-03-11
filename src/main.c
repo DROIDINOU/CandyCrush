97cf247bd029d04b261d67d1617f832c6b5be4e7
DROIDINOU <76789720+DROIDINOU@users.noreply.github.com>
2025-03-11 18:10:01 +0100

    trying to display messages

--- src/main.c
+++ src/main.c
@@ -203,3 +203,161 @@ int main()
 
     return 0;
 }
+
+/* MESSAGE D ENTREE FONCTIONNE MAIS PLUS LE RESTE #include "raylib.h"
+#include "matrice.h"
+#include "main.h"
+#include "affichage.h"
+#include "queue.h"
+#include "constante.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <unistd.h>
+
+// extern void FreeConsole(void);
+//  cd /C/Users/32471/MySDL2
+//   gcc main.c matrice.c constante.c queue.c affichage.c -o mon_programme -lraylib -mwindows
+
+int main()
+{
+    // FreeConsole(); // Cache la console sous Windows
+    srand(time(NULL)); // Initialisation du RNG
+    InitWindow(1000, 1000, "Candy Crush Clone");
+    SetTargetFPS(60); // Limiter à 60 FPS
+
+    // === Écran de Démarrage ===
+    bool jeuDemarre = false;
+
+    while (!WindowShouldClose() && !jeuDemarre)
+    {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+
+        DrawText("CANDY CRUSH CLONE", 350, 400, 40, DARKBLUE);
+        DrawText("Appuyez sur ESPACE pour commencer", 300, 500, 30, DARKGRAY);
+
+        if (IsKeyPressed(KEY_SPACE))
+        { // Quand ESPACE est pressé, démarre le jeu
+            jeuDemarre = true;
+        }
+
+        EndDrawing();
+    }
+
+    // === CHARGEMENT DES RESSOURCES ===
+    Texture2D textures[5];
+    textures[0] = LoadTexture("../candyimages/5cd560a569ed85884c879cb1da8e7d68.png");
+    textures[1] = LoadTexture("../candyimages/poignee_de_bonbons.png");
+    textures[2] = LoadTexture("../candyimages/kisspng-candy-crush-saga-candy-crush-soda-saga-candy-crush-candy-crush-5ad0dcad6773e1.4200818515236374214238.png");
+    textures[3] = LoadTexture("../candyimages/580b57fcd9996e24bc43c517.png");
+    textures[4] = LoadTexture("../candyimages/kisspng-candy-crush-saga-candy-crush-soda-saga-lollipop-ga-sweet-cheats-for-candy-crush-saga-1-2-ipa-war4-5b67aab6d41443.6117650115335205668687.png");
+
+    for (int i = 0; i < 5; i++)
+    {
+        if (textures[i].id == 0)
+        {
+            printf("Erreur de chargement de la texture %d\n", i);
+        }
+    }
+
+    // === Initialisation du Jeu ===
+    int niveau = 0;
+    GrilleBonbons maGrille;
+    maGrille.lignes = TAILLE;
+    maGrille.colonnes = TAILLE;
+
+    Queue q;
+    initialiser_queue(&q);
+    maGrille.estVerifiee = 0;
+
+    Actions initAction = {"INITIALISATION", {0, 0}, {0, 0}, true};
+    enfiler(&q, initAction);
+
+    bool attenteClics = false;
+    int clicCompteur = 0;
+    int coordonneesClic[4] = {-1, -1, -1, -1};
+
+    // === BOUCLE PRINCIPALE DU JEU ===
+    while (!WindowShouldClose() && niveau < 1)
+    {
+        if (q.taille > 0)
+        {
+            Actions action = defiler(&q);
+            maGrille.suppressionsRestantes = 0;
+            strcpy(maGrille.lastAction, action.actionName);
+
+            if (strcmp(action.actionName, "CALCUL") == 0)
+            {
+                printf("Traitement de CALCUL\n");
+                Calcul(&q, &maGrille, &action.pion1.x, &action.pion1.y, &action.pion2.x, &action.pion2.y);
+            }
+            else if (strcmp(action.actionName, "SUPPRESSIONV") == 0)
+            {
+                printf("Traitement de SUPPRESSIONV\n");
+                SuppressionV(&maGrille, &action.pion1.x, &action.pion1.y, &action.pion2.x, &action.pion2.y, &q);
+            }
+            else if (strcmp(action.actionName, "SUPPRESSIONH") == 0)
+            {
+                printf("Traitement de SUPPRESSIONH\n");
+                SuppressionH(&maGrille, &action.pion1.x, &action.pion1.y, &action.pion2.x, &action.pion2.y, &q);
+            }
+            else if (strcmp(action.actionName, "VERIFICATION") == 0)
+            {
+                printf("Traitement de VERIFICATION\n");
+                Verification(&maGrille, &q);
+                maGrille.estVerifiee = 1;
+            }
+            else if (strcmp(action.actionName, "DEPLACEMENT") == 0)
+            {
+                printf("Traitement de DEPLACEMENT\n");
+                maGrille.estVerifiee = 0;
+                Deplacement(&q, &maGrille, action.pion1.x, action.pion1.y, action.pion2.x, action.pion2.y);
+            }
+            else if (strcmp(action.actionName, "INITIALISATION") == 0)
+            {
+                printf("Initialisation\n");
+                initialiser_grille(&maGrille);
+                Calcul(&q, &maGrille, &action.pion1.x, &action.pion1.y, &action.pion2.x, &action.pion2.y);
+            }
+        }
+
+        // === AFFICHAGE DU JEU ===
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        afficher_grille(&maGrille, textures, &q);
+        EndDrawing();
+
+        // Vérification de fin du niveau
+        bool gelatinePresente = false;
+        for (int i = 0; i < maGrille.lignes; i++)
+        {
+            for (int j = 0; j < maGrille.colonnes; j++)
+            {
+                if (maGrille.tableau[i][j].gelatine)
+                {
+                    gelatinePresente = true;
+                    break;
+                }
+            }
+            if (gelatinePresente)
+                break;
+        }
+        if (!gelatinePresente)
+        {
+            niveau++;
+            printf("NIVEAU TERMINE\n");
+        }
+    }
+
+    // === LIBÉRATION DES RESSOURCES ===
+    for (int i = 0; i < 5; i++)
+    {
+        UnloadTexture(textures[i]);
+    }
+    CloseWindow();
+    printf("FIN DU JEU\n");
+
+    return 0;
+}*/
