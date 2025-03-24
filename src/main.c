b725e75fcd49c9dbf355e1d3d4ca7a8399873c87
DROIDINOU <76789720+DROIDINOU@users.noreply.github.com>
2025-03-24 20:36:45 +0100

    sound fixed

--- src/main.c
+++ src/main.c
@@ -8,95 +8,89 @@
 #include <stdlib.h>
 #include <time.h>
 #include <string.h>
-#include <unistd.h>
+
+// === Compilation ===
+// gcc main.c matrice.c constante.c queue.c affichage.c -o mon_programme -lraylib
+// ./mon_programme.exe
 
 int main()
 {
-    // Initialisation du générateur de nombres aléatoires
     srand(time(NULL));
-    Sound sound;
-    Music music;
-    // Initialisation de la fenêtre
     InitWindow(1000, 1000, "Candy Crush Clone");
     InitAudioDevice();
 
-    sound = LoadSound("../assets/hope.mp3");
-    music = LoadMusicStream("../assets/hope.mp3");
+    Sound sound = LoadSound("../assets/gaming-music-8-bit-console-play-background-intro-theme-278382.mp3");
+    Music music = LoadMusicStream("../assets/gaming-music-8-bit-console-play-background-intro-theme-278382.mp3");
     PlayMusicStream(music);
 
-    // Initialisation de l'audio
+    SetTargetFPS(60);
 
-    SetTargetFPS(60); // Limiter à 60 FPS
-
-    // === Écran de Démarrage ===
     bool jeuDemarre = false;
 
     while (!WindowShouldClose() && !jeuDemarre)
     {
         UpdateMusicStream(music);
-
         BeginDrawing();
         ClearBackground(RAYWHITE);
-
         DrawText("CANDY CRUSH CLONE", 350, 400, 40, DARKBLUE);
         DrawText("Appuyez sur ESPACE pour commencer", 300, 500, 30, DARKGRAY);
 
         if (IsKeyPressed(KEY_SPACE))
-        { // Quand ESPACE est pressé, démarre le jeu
             jeuDemarre = true;
-        }
 
         EndDrawing();
     }
 
-    // Chargement des textures
     Texture2D textures[5];
-    textures[0] = LoadTexture("../candyimages/5cd560a569ed85884c879cb1da8e7d68.png");                                                                             // Violet
-    textures[1] = LoadTexture("../candyimages/poignee_de_bonbons.png");                                                                                           // Rouge
-    textures[2] = LoadTexture("../candyimages/kisspng-candy-crush-saga-candy-crush-soda-saga-candy-crush-candy-crush-5ad0dcad6773e1.4200818515236374214238.png"); // Bleu
-    textures[3] = LoadTexture("../candyimages/580b57fcd9996e24bc43c517.png");                                                                                     // Vert
+    textures[0] = LoadTexture("../candyimages/5cd560a569ed85884c879cb1da8e7d68.png");
+    textures[1] = LoadTexture("../candyimages/poignee_de_bonbons.png");
+    textures[2] = LoadTexture("../candyimages/kisspng-candy-crush-saga-candy-crush-soda-saga-candy-crush-candy-crush-5ad0dcad6773e1.4200818515236374214238.png");
+    textures[3] = LoadTexture("../candyimages/580b57fcd9996e24bc43c517.png");
     textures[4] = LoadTexture("../candyimages/kisspng-candy-crush-saga-candy-crush-soda-saga-lollipop-ga-sweet-cheats-for-candy-crush-saga-1-2-ipa-war4-5b67aab6d41443.6117650115335205668687.png");
 
     for (int i = 0; i < 5; i++)
     {
         if (textures[i].id == 0)
-        {
             printf("Erreur de chargement de la texture %d\n", i);
-        }
     }
 
-    // Un seul niveau pour l'exemple
     int niveau = 0;
-
-    // Initialisation de la grille et de la queue
     GrilleBonbons maGrille;
     maGrille.lignes = TAILLE;
     maGrille.colonnes = TAILLE;
+    maGrille.estVerifiee = 0;
 
     Queue q;
     initialiser_queue(&q);
-    maGrille.estVerifiee = 0;
-
-    // Ajout d'une action d'initialisation
     Actions initAction = {"INITIALISATION", {0, 0}, {0, 0}, true};
     enfiler(&q, initAction);
-    Actions action;
 
     bool attenteClics = false;
     int clicCompteur = 0;
     int coordonneesClic[4] = {-1, -1, -1, -1};
 
-    // Boucle principale Raylib
+    // Timer d'attente
+    bool etatAttente = false;
+    double tempsDebutAttente = 0.0;
+    double dureeAttente = 0.5; // 0.5 secondes
+
     while (!WindowShouldClose() && niveau < 1)
     {
         UpdateMusicStream(music);
 
-        // Traitement d'une action par frame (si la file n'est pas vide)
-        if (q.taille > 0)
+        // Si on est en attente d'une pause (remplaçant usleep)
+        if (etatAttente)
+        {
+            if ((GetTime() - tempsDebutAttente) >= dureeAttente)
+            {
+                etatAttente = false; // fin de l'attente, traitement peut reprendre
+            }
+        }
+        else if (q.taille > 0)
         {
             Actions action = defiler(&q);
             maGrille.suppressionsRestantes = 0;
-            strcpy(maGrille.lastAction, action.actionName); // On mémorise l'action traitée
+            strcpy(maGrille.lastAction, action.actionName);
 
             if (strcmp(action.actionName, "CALCUL") == 0)
             {
@@ -107,13 +101,15 @@ int main()
             {
                 printf("Traitement de SUPPRESSIONV\n");
                 SuppressionV(&maGrille, &action.pion1.x, &action.pion1.y, &action.pion2.x, &action.pion2.y, &q);
-                usleep(500 * 1000);
+                etatAttente = true;
+                tempsDebutAttente = GetTime();
             }
             else if (strcmp(action.actionName, "SUPPRESSIONH") == 0)
             {
                 printf("Traitement de SUPPRESSIONH\n");
                 SuppressionH(&maGrille, &action.pion1.x, &action.pion1.y, &action.pion2.x, &action.pion2.y, &q);
-                usleep(500 * 1000);
+                etatAttente = true;
+                tempsDebutAttente = GetTime();
             }
             else if (strcmp(action.actionName, "VERIFICATION") == 0)
             {
@@ -131,7 +127,7 @@ int main()
             {
                 printf("Traitement de LECTURE via clics de souris\n");
                 attenteClics = true;
-                clicCompteur = 0; // Réinitialise le compteur pour les deux clics
+                clicCompteur = 0;
             }
             else if (strcmp(action.actionName, "INITIALISATION") == 0)
             {
@@ -141,28 +137,20 @@ int main()
             }
         }
 
-        // Si on est en mode "LECTURE" par clic, on vérifie les clics de souris
         if (attenteClics)
         {
             if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
             {
-                // Récupération de la position de la souris
                 Vector2 posSouris = GetMousePosition();
-
-                // Calcul des positions tel que dans afficher_grille
                 int tailleCase = 50;
-                int largeurFenetre = 1000;
-                int hauteurFenetre = 1000;
                 int grilleLargeur = maGrille.colonnes * tailleCase;
                 int grilleHauteur = maGrille.lignes * tailleCase;
-                int offsetX = (largeurFenetre - grilleLargeur) / 2;
-                int offsetY = (hauteurFenetre - grilleHauteur) / 2;
+                int offsetX = (1000 - grilleLargeur) / 2;
+                int offsetY = (1000 - grilleHauteur) / 2;
 
-                // Position relative dans la grille
                 int xRel = posSouris.x - offsetX;
                 int yRel = posSouris.y - offsetY;
 
-                // Vérifier que le clic se situe bien dans la zone de la grille
                 if (xRel >= 0 && xRel < grilleLargeur && yRel >= 0 && yRel < grilleHauteur)
                 {
                     int colonne = xRel / tailleCase;
@@ -171,7 +159,7 @@ int main()
                     coordonneesClic[clicCompteur * 2 + 1] = colonne;
                     clicCompteur++;
                     printf("Clic %d: ligne=%d, colonne=%d\n", clicCompteur, ligne, colonne);
-                    // Une fois les deux clics effectués, appeler la fonction de lecture
+
                     if (clicCompteur == 2)
                     {
                         LirePionsAChanger(&maGrille, coordonneesClic[0], coordonneesClic[1],
@@ -186,15 +174,13 @@ int main()
             }
         }
 
-        // AFFICHAGE A CHAQUE FRAME
         BeginDrawing();
         ClearBackground(RAYWHITE);
         afficher_grille(&maGrille, textures, &q);
         EndDrawing();
 
-        // Vérification de fin du niveau (ex: plus de gélatine ?)
         bool gelatinePresente = false;
-        for (int i = 0; i < maGrille.lignes; i++)
+        for (int i = 0; i < maGrille.lignes && !gelatinePresente; i++)
         {
             for (int j = 0; j < maGrille.colonnes; j++)
             {
@@ -204,27 +190,20 @@ int main()
                     break;
                 }
             }
-            if (gelatinePresente)
-                break;
         }
+
         if (!gelatinePresente)
         {
             niveau++;
             printf("NIVEAU TERMINE\n");
         }
-
-        // Mise à jour de la musique
     }
 
-    // Libération des ressources
     for (int i = 0; i < 5; i++)
-    {
         UnloadTexture(textures[i]);
-    }
-    StopMusicStream(music);
 
+    StopMusicStream(music);
     CloseAudioDevice();
-    // Fermeture de l'audio et de la fenêtre
     CloseWindow();
 
     printf("FIN DU JEU\n");
