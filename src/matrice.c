ea310eb91c6adbdf9d1bd48ff21e7119c285ca12
DROIDINOU <76789720+DROIDINOU@users.noreply.github.com>
2025-03-31 02:13:39 +0200

    details

--- src/matrice.c
+++ src/matrice.c
@@ -92,27 +92,6 @@ void initialiser_grille(GrilleBonbons *grille)
     initialiserGelatines(grille); // initialise la grille de gelatines
 }
 
-// a ameliorer et a deplacer dans main ou creer fichier
-int ObtenirReponseAuMessage(const char message[][3][MAXLONGUEUR], int index)
-{
-    int reponse;
-    do
-    {
-        printf("%s", message[index][0]);     // Affiche le message
-        int result = scanf(" %d", &reponse); // Lit un seul caractère
-        while (getchar() != '\n')
-            ;
-
-        // Vérifie que la réponse est valide
-        if (result == -1)
-        {
-            // printf("Reponse invalide. Essayez encore.\n");
-        }
-    } while (reponse < 1 || reponse > 21); // Si la réponse n'est pas valide, on redemande
-
-    return reponse - 1;
-}
-
 void Deplacement(Queue *q, GrilleBonbons *grille, int coordonneeXPremierPion,
                  int coordonneeYPremierPion, int coordonneeXDeuxiemePion,
                  int coordonneeYDeuxiemePion)
