1d09f186910849b74388dc577142059f6c0d3838
DROIDINOU <76789720+DROIDINOU@users.noreply.github.com>
2025-04-14 18:36:58 +0200

    details

--- src/cmdcommandes.c
+++ src/cmdcommandes.c
@@ -1,8 +1,8 @@
 // del /p *.exe
-//  affichage.c main.c matrice.c constante.c queue.c
 // gcc -o sas affichage.c main.c matrice.c constante.c queue.c erreur.c generationaleatoire.c
 // cls
 
 // a faire ne pas supprimer gelatines a initialisation et
 // les faire descendre ???? ou les gerer au niveau de la structure de niveau
-// calcul doit etre lance a partir de initialiser grille
+// dans deplacement creer fonction de verification pour pion deplacement voisin avec abs
+//
